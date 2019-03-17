7c8ec954622ed2daf73a03b35e99a2cc22dfc8ba
micahwierenga <wierenga.micah@gmail.com>
2019-03-16 20:07:53 -0600

    Removes answers from script.js

--- script.js
+++ script.js
@@ -9,34 +9,29 @@
 // so they don't throw errors for the exercises that follow.
 
 // 1. A light switch that can be either on or off.
-// Boolean
+
 // 2. A user's email address.
-// String
+
 // 3. A spaceship with a hull, laser blasters, tractor beam, and warp drive.
-// Object
+
 // 4. A list of student names from our class.
-// Array
+
 // 5. A list of student names from our class, each with a location.
-// Array of Objects
+
 // 6. A list of student names from our class, each with a location and a list of favorite tv shows.
-// Array of objects
+
 
 
 // CREATING AND ACCESSING SIMPLE ARRAYS AND SIMPLE OBJECTS
 // 1. Create a variable whose value is an array that holds all of the colors of the rainbow.
-var rainbowColors = ['red','orange','yellow','green','blue','purple'];
+
 // 2. Write code that will access "blue" from the rainbow array. Log it to the console to test.
-rainbowColors[4];
+
 // 3. Make an object whose variable name is your name and holds the information about
 // your favorite food, a hobby, the name of your current city, and your favorite data type.
-var johnSmith = {
-	favoriteFood: 'Chinese',
-	hobby: 'Toy collecting',
-	town: 'Denver',
-	favoriteDatatype: 'String'
-}
+
 // 4. Write code that will access your hobby from the object that you just created.
-jasonSpiller.hobby;
+
 
 
 // ACCESSING VALUES FROM COMPLEX OBJECTS
@@ -86,15 +81,15 @@ var crazyObject = {
 
 
 // 1. "omg my mouth is burning"
-console.log(crazyObject.taco[1].salsa[5]);
+
 // 2. "Pretty pretty prettayyyyy good"
-console.log(crazyObject.larry.quotes[0]);
+
 // 3. "Swearing at Larry and Jeff"
-console.log(crazyObject.larry.characters[2].favourtieHobby);
+
 // 4. "Chicken Teriyaki Boyyyyyy"
-console.log(crazyObject.larry.nicknames[1]);
+
 // 5. The entire object the contains the name funkhauser
-console.log(crazyObject.larry.characters[1]);
+
 
 
 // MANIPULATING COMPLEX OBJECTS
@@ -118,6 +113,4 @@ var inception = {
 
 // Change the value of "limbo" from "Joseph Gordon Levitt" to null (no quotes around null).
 // Now log the "limbo" property to the console.
-inception.reality.dreamLayer1.dreamLayer2.dreamLayer3.dreamLayer4.dreamLayer5.dreamLayer6.limbo = null;
 
-console.log(inception.reality.dreamLayer1.dreamLayer2.dreamLayer3.dreamLayer4.dreamLayer5.dreamLayer6.limbo);
